ed29427a6d87cc7bf0ca7b660c5522cc05256313
hunnypotpy <sarahdraws@gmail.com>
2021-02-12 17:18:42 -0800

    cleaned code, added while loop

--- magic8ball.py
+++ magic8ball.py
@@ -10,9 +10,11 @@ def Magic8Ball():
         print("Welcome to Magic 8 Ball!")
         question = input("Please ask the Magic 8 Ball your question: ")
         print("SHAKE")
-        time.sleep(3)
+        time.sleep(2)
         print("SHAKE")
-        time.sleep(3)
+        time.sleep(2)
+        print("SHAKE")
+        time.sleep(2)
         
         fortunes = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
         answer = random.choice(fortunes)
